122c30e1f0f1ac07858cabdea34c83af47fde2dd
Sam Mead <Sam@Sams-MacBook-Pro.local>
2016-04-22 17:55:54 +0800

    New post: sticky footer

---
+++ js/fetcher.js
@@ -0,0 +1,9 @@
+// Testing APIs with JS
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.typeform.com/v1/form/kHkmFu?key=3428ac4f1f2fc402a0a25b8d5564815e5e7cbfc7");
+// Add your code below!
+xhr.send();
+
+console.log(xhr.status);
+console.log(xhr.statusText);
