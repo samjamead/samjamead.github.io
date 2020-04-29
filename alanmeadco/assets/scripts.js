c8c12abaa17dbcd3ea3a5b2ae978c7e1b0a1ec5b
Sam Mead <samjamead@gmail.com>
2020-04-29 18:39:21 +0100

    Remove Forestry.io settings and testing directories

--- alanmeadco/assets/scripts.js
+++
@@ -1,9 +0,0 @@
-jQuery( document ).ready(function($){
-  // Smoooooth scrolling
-  $('.smooth-scroll').click(function(){
-    $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top - 60
-    }, 500);
-    return false;
-  });
-});
