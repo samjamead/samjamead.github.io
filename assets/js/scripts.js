174ae9825b42c897ac664c557c972f88a59bb068
Sam Mead <samjamead@gmail.com>
2022-03-07 16:06:04 +0000

    v8.1 :scream:

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,39 +1,18 @@
 jQuery(document).ready(function($){
 
-  // Active page highlight
-  var bodyid = $("body").attr('id');
-  $("#nav-" + bodyid).addClass('active');
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://public.ecologi.com/users/samuelmead/trees",
+    "method": "GET",
+    "headers": {
+      "Content-Type": "application/json"
+    }
+  };
 
-  // Dynamically set bottom margin of body for sticky footer
-  stickyFooter();
-
-  // Add target="_blank" to all external links
-  $("a[href^='http://']").attr("target","_blank");
-  $("a[href^='https://']").attr("target","_blank");
-
-  // Unless it's because of Github pages
-  $("a[href^='http://samjamead.github.io/']").attr("target","_self");
-  $("a[href^='https://samjamead.github.io/']").attr("target","_self");
-
-  // Open the menu
-  var $hamburger = $('.hamburger');
-  $hamburger.on("click", function(){
-    $hamburger.toggleClass("is-active");
-    $('.nav-container').toggleClass("menu-open");
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    $(".treespan").html(response.total);
   });
 
 });
-
-// Trigger sizing functions on window resize
-$( window ).resize(function() {
-  stickyFooter();
-});
-
-// Dynamically set bottom margin of body for sticky footer
-function stickyFooter() {
-  setTimeout (function () {
-    var bodyBottomMargin = $("footer").outerHeight();
-    console.log(bodyBottomMargin);
-    $("body").css("margin-bottom", bodyBottomMargin);
-  }, 100);
-}
