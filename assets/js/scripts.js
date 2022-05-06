285a3b3e61424986f37f0bb3e94754757ac4fd53
Sam Mead <samjamead@gmail.com>
2022-05-06 17:12:21 +0100

    Grammar, feedback, typos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,6 +7,15 @@ jQuery(document).ready(function($){
     $('.nav-container').toggleClass("menu-open");
   });
 
+  var $shakee = $('.shakee');
+  $('.shaker').on("click", function(){
+    console.log("Clicked!");
+    $shakee.addClass("shakeX").delay(1500).queue(function(next){
+      $(this).removeClass("shakeX");
+      next();
+    });
+  });
+
   const settings = {
     "async": true,
     "crossDomain": true,
