e1fdbdc273424feac7c6fa964372ff17f54940fe
Sam Mead <samjamead@gmail.com>
2023-05-02 12:16:35 +0100

    Poking icons around

--- components/blog/listItem.js
+++ components/blog/listItem.js
@@ -1,12 +1,21 @@
+import Image from "next/image";
+import SnowIcon from "../../public/img/cloud-snow.svg";
+import CalendarIcon from "../../public/img/calendar-range.svg";
+
 export default function ({ data, children }) {
   return (
     <li key={data.id} className="list-item-wrapper">
-      <div className="list-item-icon"></div>
+      <div className="list-item-icon">
+        <Image src={CalendarIcon} alt="Weather at the time of posting" />
+      </div>
       <div className="list-item-inner">
         <div className="list-item-header">
-          <p>
-            {data.type ? data.type : "Post"} &mdash; {data.date}
-          </p>
+          <div>
+            <p>{data.date}</p>
+          </div>
+          <div>
+            <p>{data.type ? data.type : "Post"}</p>
+          </div>
         </div>
         <div className="list-item-content">{children}</div>
       </div>
